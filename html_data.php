3365f6109e7f90d473ab322669245952984083fe
Metehan Erkan <70102038+bymerkan@users.noreply.github.com>
2022-02-22 14:20:23 +0300

    create html_data.php

---
+++ html_data.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+
+GENERAL FORMAT FOR DATA
+
+['ID','1st Part','2nd Part','3rd Part','Column Names','Height of Text Frame','Class for 3rd Part']
+
+
+*** Length Unit is mm for images.
+
+Each element in 3rd Part (e.g. Cell Element 1) will be placed to table.
+
+There is 2 example for Classes. (3 and 4)
+
+Image urls can be local.
+
+*/
+
+
+$data = [
+  ['1','<img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" height="35" width="97.5"\> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.','<b>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</b>','Cell Element 1,Cell Element 2,Cell Element 3,Cell Element 4,Cell Element 5','','XX','3'],
+  ['2','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Some <sub>sub</sub> and <sup>sup</sup> text.','<b>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</b>','<img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" height="7" width="8"\>,<img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" height="7" width="8"\>,<img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" height="7" width="8"\>,<img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" height="7" width="8"\>,<img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" height="7" width="8"\>','','XX','4']
+
+];
+  
+  
+
+
+
+
+?>
